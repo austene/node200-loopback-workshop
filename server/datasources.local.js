1b2cca6fbd6f8a557366ac234e7f70f85230a610
AE <austence@sbcglobal.net>
2019-05-13 09:31:57 -0700

    updates local uri.

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,16 +1,16 @@
 module.exports = {
-  'db': {
-    'name': 'db',
-    'connector': 'memory',
+  "db": {
+    "name": "db",
+    "connector": "memory",
   },
-  'MongoDB': {
-    'host': '',
-    'port': 0,
-    'url': process.env.MONGODB_URI,
-    'database': '',
-    'password': '',
-    'name': 'MongoDB',
-    'user': '',
-    'connector': 'mongodb',
+  "MongoDB": {
+    "host": "",
+    "port": 0,
+    "url": "mongodb://localhost:27017",
+    "database": "",
+    "password": "",
+    "name": "MongoDB",
+    "user": "",
+    "connector": "mongodb",
   },
 };
