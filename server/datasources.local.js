a73dc4cf77e0a95f62e017f5596e995c77bb59d7
AE <austence@sbcglobal.net>
2019-05-13 10:48:49 -0700

    updates middleware and local datasources url

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,17 +1,11 @@
-"use strict";
 module.exports = {
-  "db": {
-    "name": "db",
-    "connector": "memory",
+  'db': {
+    'name': 'db',
+    'connector': 'memory',
   },
-  "MongoDB": {
-    "host": "",
-    "port": 0,
-    "url": "mongodb://localhost:27017",
-    "database": "",
-    "password": "",
-    "name": "MongoDB",
-    "user": "",
-    "connector": "mongodb",
+  'MongoDB': {
+    'url': process.env.MONGODB_URI,
+    'name': 'MongoDB',
+    'connector': 'mongodb',
   },
 };
